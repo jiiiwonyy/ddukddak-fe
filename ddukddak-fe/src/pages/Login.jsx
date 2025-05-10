79f1e66c944fe37b312585d4bc3593707afd0baa
jiiiwonyy <ji__won_22@naver.com>
2025-05-11 03:06:27 +0900

    feat: loginpage

---
+++ ddukddak-fe/src/pages/Login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+const KakaoButton = styled.button`
+  width: 100%;
+  height: 3.5rem;
+  padding: 0.5rem 1rem;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  background-color: #ffe81a;
+  border-radius: 2rem;
+  border: none;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  gap: 3rem;
+  padding: 0 20px;
+`;
+
+const Login = () => {
+  return (
+    <LoginContainer>
+      <img src="assets/images/login.svg" alt="Login" />
+      <p className="title1">서비스이름</p>
+      <KakaoButton className="title3">
+        <img src="assets/images/KakaoTalk.svg" alt="loginButton" /> 카카오로
+        로그인하기
+      </KakaoButton>
+    </LoginContainer>
+  );
+};
+export default Login;
