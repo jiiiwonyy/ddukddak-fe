d63c648fb608e9f824f937a73b63215d0660bc4e
jiiiwonyy <ji__won_22@naver.com>
2025-05-21 13:38:37 +0900

    feat: 카카오 로그인 초기 세팅

--- ddukddak-fe/src/pages/Login.jsx
+++ ddukddak-fe/src/pages/Login.jsx
@@ -2,6 +2,40 @@ import React from "react";
 import styled from "styled-components";
 import PageWrapper from "../components/PageWrapper";
 
+const Login = () => {
+  const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const handleLogin = () => {
+    window.location.href = link;
+  };
+
+  return (
+    <PageWrapper>
+      <LoginContainer>
+        <img src="assets/images/login.svg" alt="Login" />
+        <p className="title1">서비스이름</p>
+        <KakaoButton className="title3" onClick={handleLogin}>
+          <img src="assets/images/KakaoTalk.svg" alt="loginButton" /> 카카오로
+          로그인하기
+        </KakaoButton>
+      </LoginContainer>
+    </PageWrapper>
+  );
+};
+export default Login;
+
+const LoginContainer = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  padding: 0 20px;
+  margin-top: 100px;
+`;
+
 const KakaoButton = styled.button`
   width: 100%;
   height: 3.5rem;
@@ -13,24 +47,3 @@ const KakaoButton = styled.button`
   border-radius: 2rem;
   border: none;
 `;
-
-const LoginContainer = styled(PageWrapper)`
-  align-items: center;
-  justify-content: center;
-  gap: 3rem;
-  padding: 0 20px;
-`;
-
-const Login = () => {
-  return (
-    <LoginContainer>
-      <img src="assets/images/login.svg" alt="Login" />
-      <p className="title1">서비스이름</p>
-      <KakaoButton className="title3">
-        <img src="assets/images/KakaoTalk.svg" alt="loginButton" /> 카카오로
-        로그인하기
-      </KakaoButton>
-    </LoginContainer>
-  );
-};
-export default Login;
