fc5f81187079ef1a0cc2896aa9e8bfaa8288f3c8
jiiiwonyy <ji__won_22@naver.com>
2025-06-03 01:36:49 +0900

    vercel 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -5,70 +5,78 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url)); // __dirname처럼 사용
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-  server: {
-    host: true,
-    port: 5174,
-    strictPort: true, // 포트가 사용 중이면 서버 시작 실패
-    https: {
+export default defineConfig(({ mode }) => {
+  // https 옵션 개발환경에서만 세팅
+  const isDev = mode === "development";
+  let https = false;
+  if (isDev) {
+    https = {
       key: fs.readFileSync(path.resolve(__dirname, "localhost+2-key.pem")),
       cert: fs.readFileSync(path.resolve(__dirname, "localhost+2.pem")),
+    };
+  }
+
+  return {
+    server: {
+      host: true,
+      port: 5174,
+      strictPort: true,
+      https, // 개발때만 https 세팅, 배포 때는 false
+      hmr: false,
     },
-    hmr: false,
-  },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "robots.txt",
-        "apple-touch-icon.png",
-        "icons/*.png",
-      ],
-      manifest: {
-        name: "Ddukddak App",
-        short_name: "Ddukddak",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        icons: [
-          {
-            src: "/icons/pwa-icon-48.png",
-            sizes: "48x48",
-            type: "image/png",
-          },
-          {
-            src: "/icons/pwa-icon-72.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "/icons/pwa-icon-96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "/icons/pwa-icon-144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "/icons/pwa-icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icons/pwa-icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "favicon.svg",
+          "favicon.ico",
+          "robots.txt",
+          "apple-touch-icon.png",
+          "icons/*.png",
         ],
-      },
-    }),
-  ],
+        manifest: {
+          name: "Ddukddak App",
+          short_name: "Ddukddak",
+          start_url: "/",
+          display: "standalone",
+          background_color: "#ffffff",
+          icons: [
+            {
+              src: "/icons/pwa-icon-48.png",
+              sizes: "48x48",
+              type: "image/png",
+            },
+            {
+              src: "/icons/pwa-icon-72.png",
+              sizes: "72x72",
+              type: "image/png",
+            },
+            {
+              src: "/icons/pwa-icon-96.png",
+              sizes: "96x96",
+              type: "image/png",
+            },
+            {
+              src: "/icons/pwa-icon-144.png",
+              sizes: "144x144",
+              type: "image/png",
+            },
+            {
+              src: "/icons/pwa-icon-192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/icons/pwa-icon-512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
